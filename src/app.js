e146db08c0352edaa1489b41dd3bd0f54e54f455
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-11 05:12:13 +0000

    avances

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let btnChange = document.getElementById("button-change");
+
+  let cardNumber = document.getElementsByClassName("number"); //number top es [0] y inferior [1]
+
+  let centerSymbol = document.querySelector(".center"); // simbolos del centro
+
+  let otherSymbol = document.querySelector(".symbol-top"); // simbolos del header
+
+  let otherSymbol2 = document.querySelector(".symbol-botom"); // simbolos del footer
+
+  let changeCard = document.getElementById("button-change");
+
+  let arrayCards = ["♦", "♥", "♠", "♣"];
+
+  changeCard.addEventListener("click", function() {
+    let symbolNum = Math.floor(Math.random() * 4);
+    centerSymbol.innerHTML = arrayCards[symbolNum];
+    otherSymbol.innerHTML = arrayCards[symbolNum];
+    otherSymbol2.innerHTML = arrayCards[symbolNum];
+  });
 };
